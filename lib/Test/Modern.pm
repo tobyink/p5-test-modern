9380e2eedc3dcd7de347e71cbe1d5e44fdc4aba5
Toby Inkster <mail@tobyinkster.co.uk>
2014-03-10 12:43:58 +0000

    bump version; changelog

--- lib/Test/Modern.pm
+++ lib/Test/Modern.pm
@@ -5,7 +5,7 @@ use warnings;
 package Test::Modern;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.000_06';
+our $VERSION   = '0.000_07';
 
 use Exporter::Tiny   0.030 qw();
 use Import::Into     1.002 qw();
